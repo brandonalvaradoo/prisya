eff7b5c46c3bb8bc1fca555f7e3bc8d361d6bcd4
BRANDON LEONARDO URIBE ALVARADO <105460532+brandonalvaradoo@users.noreply.github.com>
2026-04-14 22:28:44 -0600

    Fase 0: Interfaz

--- packages/scry/volk/volks/map.php
+++ packages/scry/volk/volks/map.php
@@ -1,8 +1,8 @@
 <?php
-// Map updated on 15/04/2026 05:57:12
+// Map updated on 15/04/2026 06:25:57
 
 return array (
-  'attr' => 'C:\\xampp\\htdocs\\superpris\\packages\\scry\\volk\\volks\\attr\\0208d49e.php',
+  'attr' => 'C:\\xampp\\htdocs\\prisya\\packages\\scry\\volk\\volks\\attr\\9292c88b.php',
   'class' => 
   array (
   ),
